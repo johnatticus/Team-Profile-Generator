e1d6a373b8cfad2d84a5da7132b4c5015a898b26
John Hysong <johnahysong@gmail.com>
2022-04-24 01:00:34 -0400

    added boilerplate files. worked on styling

---
+++ test/Employee.test.js
@@ -0,0 +1,18 @@
+// require the employee from the lib folder
+const Employee = require('../lib/Employee')
+
+// test can begin employee instance
+describe("Employee", () => {
+    it("Begin the instance of a new employee", () => {
+        const e = new Employee();
+        expect(new Employee(e)).toBe("object");
+    });
+});
+
+// test can set name via constructor args
+// can set id via constructor args
+// can set email via constructor args
+// can set name via constructor args
+// can set id via constructor function
+// can get email via constructor function
+// role functrion should return employee
